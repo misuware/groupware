b462598996717f3f3df1e3a1b44e41f89e9ff70f
lmj <mjlee95613@gmail.com>
2024-06-07 12:14:27 +0900

    push 테스트

---
+++ src/main/java/groupware/com/usr/service/vo/UserVO.java
@@ -0,0 +1,11 @@
+package groupware.com.usr.service.vo;
+
+import lombok.Data;
+
+@Data
+public class UserVO {
+    private String userId;
+    private String userPassword;
+    private String authCd;
+}
+
